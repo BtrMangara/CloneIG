03b78f6e7e8b00935253385793fa99b995e649df
Mangara Butarbutar <120437609+BtrMangara@users.noreply.github.com>
2023-10-28 23:30:54 +0700

    dummy done

--- client/codiagram/src/components/post.js
+++ client/codiagram/src/components/post.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal'
+import { BsThreeDots } from 'react-icons/bs';
+import {GrUpdate} from 'react-icons/gr';
 
 const Post = () => {
+
+    const [showModals, setshowModals] = useState(false);
     return (
         <div className='mt-5'>
-            Hello World
+            <div className="container text-center">
+            <div className="row mt-5">
+                
+                <div className="col icon-link-hover">
+                    <img src='https://via.placeholder.com/500' className='img-fluid' alt='' onClick={()=>setshowModals(true)}></img>
+                </div>
+                <div className="col icon-link-hover">
+                    <img src='https://via.placeholder.com/500' className='img-fluid' alt='' onClick={()=>setshowModals(true)}></img>
+                </div>
+                <div className="col icon-link-hover">
+                    <img src='https://via.placeholder.com/500' className='img-fluid' alt='' onClick={()=>setshowModals(true)}></img>
+                </div>
+                <Modal
+                    size="lg"
+                    show={showModals}
+                    onHide={() => setshowModals(false)}
+                    aria-labelledby="example-modal-sizes-title-lg"
+                >
+                    <Modal.Header className='border border-0' closeButton>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className='row pb-2 align-items-center'>
+                            <div className='col-4'>
+                                <img src='https://via.placeholder.com/500' className='img-fluid' alt=''></img>
+                            </div>
+                            <div className='col-8'>
+                            <button className="btn btn-light dropdown-toggle btn-sm-sm float-end" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <BsThreeDots/>
+                            </button>
+                            <ul className="dropdown-menu dropdown-sm-menu ">
+                                <li><div className="dropdown-item" href="#"><GrUpdate className='me-2'/>Update</div></li>
+                            </ul>
+
+                            <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" className="rounded-circle me-2" style={{width: "30px"}}alt="Avatar" />
+                            <span className='ms-2'>BtrMangara</span>
+                            <p className='m-3' style={{textAlign:'justify'}}>Occaecat magna excepteur id est enim proident excepteur id quis qui. Lorem laboris dolor magna incididunt ullamco ex nostrud esse cillum in dolore ullamco Lorem aute. Occaecat sunt magna dolor amet proident sit magna. Consequat non tempor adipisicing ullamco consectetur reprehenderit est culpa proident amet consectetur in cupidatat reprehenderit. Veniam reprehenderit incididunt est nostrud nostrud Lorem qui deserunt nostrud sint excepteur quis proident. Id deserunt elit ad dolor ad elit pariatur reprehenderit sint eu velit culpa.</p>
+                            </div>
+                        </div>
+                    </Modal.Body>
+                </Modal> 
+                
+                
+                </div>
+            </div>
         </div>
     );
 }
--- client/codiagram/src/components/profile.js
+++ client/codiagram/src/components/profile.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Post from './post';
 import Draft from './draft';
-
+import {BsThreeDots} from'react-icons/bs';
+import {BsBookmark,BsGrid3X3Gap} from 'react-icons/bs'
+import {FiLogOut} from 'react-icons/fi'
 
 const Profile = () => {
     return (
@@ -14,6 +16,14 @@ const Profile = () => {
                             <img src="https://via.placeholder.com/500" className='img-fluid rounded-circle w-50' alt=''/>
                         </div>
                         <div className='col-7 col-lg-7 col-md-7 col-sm-5 text-start mt-lg-5'>
+                            <div className='dropdown text-end me-lg-5'>
+                            <button className="btn btn-secondary dropdown-toggle btn-light btn-sm-sm" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <BsThreeDots/>
+                            </button>
+                            <ul className="dropdown-menu dropdown-sm-menu ">
+                                <li><div className="dropdown-item text-danger" href="#"><FiLogOut className='me-2'/>Log Out</div></li>
+                            </ul>
+                            </div>
                             BtrMangara
                             <p></p>
                             Butar Butar Mangara
@@ -27,10 +37,10 @@ const Profile = () => {
                     <nav className='mt-5'>
                         <div className="nav nav-tabs justify-content-center" id="nav-tab" role="tablist">
                             <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">
-                                Post
+                                <BsGrid3X3Gap className='me-2'/>Post
                             </button>
                             <button className="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">
-                                Draft
+                               <BsBookmark className='me-2'/> Draft
                             </button>
                         </div>
                     </nav>
