83640fe769bdd64807b502859d38a8bc23dc494a
Mangara Butarbutar <120437609+BtrMangara@users.noreply.github.com>
2024-03-19 19:05:02 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require('dotenv');
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORTS || 4000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
