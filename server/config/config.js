1d5daf8d0617055031bff4441aa548684815bbac
Mangara Butarbutar <120437609+BtrMangara@users.noreply.github.com>
2024-03-19 17:11:09 +0700

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 const dotenv = (require("dotenv").config()).parsed
 
 let config = {
-    username: dotenv.DB_USERNAME,
+    // username: dotenv.DB_USERNAME,
     password: dotenv.DB_PASSWORD,
     database: dotenv.DB_NAME,
     host: dotenv.DB_HOST,
