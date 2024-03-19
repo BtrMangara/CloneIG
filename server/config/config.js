029823580fd6f80a361d2f352cbe5bbd8e671935
Mangara Butarbutar <120437609+BtrMangara@users.noreply.github.com>
2024-03-19 17:16:17 +0700

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,12 @@
 const dotenv = (require("dotenv").config()).parsed
 
 let config = {
-    // username: dotenv.DB_USERNAME,
-    password: dotenv.DB_PASSWORD,
-    database: dotenv.DB_NAME,
-    host: dotenv.DB_HOST,
-    port: dotenv.DB_PORT,
-    dialect: dotenv.DB_DIALECT,
+    username: 'postgres',
+    password: 'QXmiZsnxTiaBCvfxwxxnLBchMxrmlPeH',
+    database: 'railway',
+    host: 'roundhouse.proxy.rlwy.net',
+    port: 28233,
+    dialect: 'postgres',
     migrationStorageTableName: "sequelize_migrations",
     seederStorageTableName: "sequelize_seeds"
 };
