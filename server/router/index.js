2df6932830bf0a7abeafe69c37cbad9ea81b2276
Mangara Butarbutar <120437609+BtrMangara@users.noreply.github.com>
2024-03-20 14:59:22 +0700

    Update index.js

--- server/router/index.js
+++ server/router/index.js
@@ -8,7 +8,7 @@ router.get('/',(req,res)=>{
 
 router.get('/test',(req,res)=>{
     res.json({
-        message:`Hello ${PROCESS.ENV.DB_PASS}`
+        message:`Hello ${process.env.DB_PASS}`
     });
 })
 
